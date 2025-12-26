66669b68f089158fab5a47234af1e63c28522f77
tuturuu <zedddiezxc@gmail.com>
2025-12-26 11:49:12 +0100

    feat: move most user space apps to home manager

--- home.nix
+++ home.nix
@@ -1,5 +1,4 @@
 {
-  config,
   pkgs,
   zen-browser,
   nixdots-assets,
@@ -28,13 +27,75 @@ in
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
+    # communication
     vesktop
     telegram-desktop
+    gajim
+
+    # learn
     obsidian
+
+    # utils
+    unzip
+    pavucontrol
+    dunst
+    slurp
+    grim
+    wl-clipboard
+
+    # 4hypr
+    waybar
+    rofi
+    swww
+
+    #
+    spotify
+
+    # gaming related
+    obs-studio
+    mangohud
+    protonup-qt
+    lutris
+    bottles
+    heroic
+
+    # code utils
+    kitty
+    gemini-cli-bin
+    opencode
+    pkg-config
+    nixfmt
+    markdownlint-cli
+
+    fastfetch
+    # custom
     zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     (installCursor "Charlotte-Suzu")
     (installCursor "Hatsune-Miku")
   ];
+  programs.fzf.enable = true;
+  programs.tmux.enable = true;
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+      {
+        name = "fzf-fish";
+        src = pkgs.fishPlugins.fzf-fish.src;
+      }
+      {
+        name = "forgit";
+        src = pkgs.fishPlugins.forgit.src;
+      }
+      {
+        name = "z";
+        src = pkgs.fishPlugins.z.src;
+      }
+    ];
+  };
 
   home.pointerCursor = {
     package = installCursor "Hatsune-Miku";
@@ -44,5 +105,6 @@ in
     x11.enable = true;
   };
 
+  #git tmux
   programs.home-manager.enable = true;
 }
