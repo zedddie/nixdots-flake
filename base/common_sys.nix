eb62635147f8723901a3b719c23314d0915b1a2a
zedddie <zedddie@protonmail.com>
2025-12-29 12:15:09 +0100

    wip conf

--- base/common_sys.nix
+++ base/common_sys.nix
@@ -150,47 +150,6 @@
   # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
   # ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-  hardware.graphics = {
-    enable = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-  boot.kernelPackages = pkgs.linuxPackages;
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-    nvidiaPersistenced = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = true;
-    gsp.enable = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
   # services.xserver.enable = true;
   # services.xserver.displayManager.startx.enable = true;
   # services.xserver.windowManager.dwm = {
@@ -222,12 +181,6 @@
   ];
   virtualisation.docker = {
     enable = true;
-  };
-  programs.gamemode.enable = true;
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-    dedicatedServer.openFirewall = true;
   };
   fonts.fontconfig.enable = true;
   fonts.packages = with pkgs; [
@@ -274,15 +227,6 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      PubkeyAuthentication = "yes";
-      ChallengeResponseAuthentication = "no";
-    };
-  };
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -1,41 +1,80 @@
-#
-  # hardware.graphics = {
-  #   enable = true;
-  # };
-  #
-  # # Load nvidia driver for Xorg and Wayland
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # boot.kernelPackages = pkgs.linuxPackages;
-  #
-  # hardware.nvidia = {
-  #
-  #   # Modesetting is required.
-  #   modesetting.enable = true;
-  #   nvidiaPersistenced = true;
-  #
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   # Enable this if you have graphical corruption issues or application crashes after waking
-  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-  #   # of just the bare essentials.
-  #   powerManagement.enable = false;
-  #
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = false;
-  #
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of
-  #   # supported GPUs is at:
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-  #   # Only available from driver 515.43.04+
-  #   open = true;
-  #   gsp.enable = true;
-  #
-  #   # Enable the Nvidia settings menu,
-  #   # accessible via `nvidia-settings`.
-  #   nvidiaSettings = true;
-  #
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   package = config.boot.kernelPackages.nvidiaPackages.beta;
-  # };
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../../base/common_sys.nix
+  ];
+
+  networking.hostName = "pcnix";
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      PubkeyAuthentication = "yes";
+      ChallengeResponseAuthentication = "no";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  hardware.nvidia = {
+
+    modesetting.enable = true;
+    nvidiaPersistenced = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = true;
+    gsp.enable = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+    NIXOS_OZONE_WL = "1";
+    EDITOR = "nvim";
+  };
+  system.stateVersion = "25.11";
+}
