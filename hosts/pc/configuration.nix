9b75e5060952d14a42af8c5c1fe839cd2de65af8
zedddie <zedddie@protonmail.com>
2026-01-18 10:42:59 +0100

    feat: configure forgejo runner

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -29,34 +29,13 @@
   boot.kernelPackages = pkgs.linuxPackages;
 
   hardware.nvidia = {
-
     modesetting.enable = true;
     nvidiaPersistenced = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
     open = true;
     gsp.enable = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   virtualisation.docker.enable = true;
@@ -64,27 +43,24 @@
     package = pkgs.forgejo-runner;
     instances.default = {
       enable = true;
-      name = "my-forgejo-runner-01";
-      token = "<registration-token>";
-      url = "https://code.forgejo.org/";
-      # tokenFile = /var/lib/
+      name = "nixpc-runner";
+      url = "http://192.168.1.50:3000/";
+      token = "ui8bz627tqCKrjUiHsNwPrm07i9r9Y3VhWi1Ft2P";
       labels = [
         "node-22:docker://node:22-bookworm"
         "nixos-latest:docker://nixos/nix"
       ];
       settings = {
-        settings = {
-          log.level = "info";
-          runner = {
-            capacity = 1;
-            timeout = "3h";
-            fetch_interval = "2s";
-          };
-          container = {
-            network = "bridge";
-            privileged = false;
-            docker_host = "unix:///var/run/docker.sock";
-          };
+        log.level = "info";
+        runner = {
+          capacity = 1;
+          timeout = "3h";
+          fetch_interval = "2s";
+        };
+        container = {
+          network = "bridge";
+          privileged = false;
+          docker_host = "unix:///var/run/docker.sock";
         };
       };
     };
