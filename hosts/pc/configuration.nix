dad6d9ed36fca2383f9d3b463773fc5b5844d302
zedddie <zedddie@protonmail.com>
2026-06-19 02:06:31 +0200

    still zig creators de meow

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -108,7 +108,8 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.cinnamon.enable = true;
+  # services.xserver.desktopManager.cinnamon.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
   environment.sessionVariables = {
     EDITOR = "nvim";
